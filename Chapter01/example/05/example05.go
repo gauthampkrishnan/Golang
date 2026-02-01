b0cc480b82a5b37b462fa819ffd2f57e950414f1
gauthampkrishnan <gauthampkrishnan@ieee.org>
2026-02-01 14:50:29 -0500

    Added file handling example

---
+++ Chapter01/example/05/example05.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, file := range files {
+			f, err := os.Open(file)
+			if err != nil {
+				fmt.Println("Error", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+
+		}
+	}
+	for key, count := range counts {
+		if count > 1 {
+			fmt.Println(key, count)
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+}
